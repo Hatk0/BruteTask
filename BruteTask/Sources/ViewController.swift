3f5f998196af858a90dd0b9cde8f0e61821123c1
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-06 22:07:13 +0300

    Project customization

---
+++ BruteTask/Sources/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setup
+    
+    private func setupView() {
+        
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+
+    private func setupLayout() {
+        
+    }
+}
+
