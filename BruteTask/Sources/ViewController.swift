9c2603e7322e7e96895e7d5d7a1f06820d1b16c5
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-07 21:07:52 +0300

    Create UITextField and make constraints

--- BruteTask/Sources/ViewController.swift
+++ BruteTask/Sources/ViewController.swift
@@ -4,6 +4,13 @@ class ViewController: UIViewController {
     
     // MARK: - UI
     
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -16,15 +23,19 @@ class ViewController: UIViewController {
     // MARK: - Setup
     
     private func setupView() {
-        
+        view.backgroundColor = .systemBackground
     }
     
     private func setupHierarchy() {
-        
+        view.addSubview(textField)
     }
 
     private func setupLayout() {
-        
+        NSLayoutConstraint.activate([
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40),
+            textField.widthAnchor.constraint(equalToConstant: 200),
+        ])
     }
 }
 
